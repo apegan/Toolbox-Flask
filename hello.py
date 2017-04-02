68271c213da7633e30dbdb6fc322af1fe803af25
Alisha P <apegan@wellesley.edu>
2017-04-02 11:40:41 -0400

    attempting to create a list of questions and fields

--- hello.py
+++ hello.py
@@ -4,20 +4,33 @@ Web page that ask for the user to fill a form, checks if data was
 provided, and then displays the results.
 """
 
-from flask import Flask, render_template, request
-import os
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-poll_data = ['What is your name?',
-             'Age?',
-             'Who is your favorite SoftDes ninja?']
+poll_data = [
+    {'name': 'What is your name?'},
+    {'age': 'What is your age?'},
+    {'ninja': 'Who is your favorite ninja?'}
+]
 
 filename = 'data.txt'
 
 
 @app.route('/')
-def root():
+def home():
     return render_template('student.html', data=poll_data)
+
+
+@app.route('/login')
+def poll():
+    response = request.args.get('field')
+
+    out = open(filename, 'a')
+    out.write(response + '\n')
+    out.close()
+
+    return response
+
 
 # app.run(host='0.0.0.0')     # run on all public IPS
 # app.run(debug=True)         # server will reload on code changes
